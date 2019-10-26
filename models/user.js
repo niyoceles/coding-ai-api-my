29c4d91783ee82c21f98c07d299c6127a9f58d2a
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-10-27 00:53:51 +0200

    config(setup-database): database configuration  - setup project environmenet  - config postgres database

---
+++ models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
